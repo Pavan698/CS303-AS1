ba9fbb48e3f7d9f9715f82a7cd35427c773b3a62
Pavan Krishna Karri <pkcgb@umsystem.edu>
2023-09-11 11:11:55 -0500

    Create myarray.h

---
+++ myarray.h
@@ -0,0 +1,19 @@
+#ifndef MYARRAY_H
+#define MYARRAY_H
+
+class MyArray {
+public:
+    MyArray(); // Constructor
+    ~MyArray(); // Destructor
+    bool readFromFile(const char* filename); // Function to read data from a file
+    int find(int value); // Function to find an integer and return its index
+    std::pair<int, int> modify(int index, int newValue); // Function to modify an integer
+    void add(int newValue); // Function to add a new integer to the end
+    void replace(int index, int value); // Function to replace or remove an integer
+
+private:
+    int* data; // Dynamic array to store integers
+    int size; // Current size of the array
+};
+
+#endif
